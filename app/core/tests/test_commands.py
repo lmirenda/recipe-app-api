99b793e8cb04109f6c55c182ec54c0d16113863a
lmirenda <lucamateomirenda@gmail.com>
2023-02-02 14:56:28 -0300

    Section 6

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -9,7 +9,7 @@ from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
 
-@patch('core.management.commands.wait_for_db.Command.check')
+@patch("core.management.commands.wait_for_db.Command.check")
 class CommandTests(SimpleTestCase):
     """Test Commands"""
 
@@ -17,17 +17,18 @@ class CommandTests(SimpleTestCase):
         """Test waiting for db if db ready."""
         patched_check.return_value = True
 
-        call_command('wait_for_db')
+        call_command("wait_for_db")
 
-        patched_check.assert_called_once_with(databases=['default'])
+        patched_check.assert_called_once_with(databases=["default"])
 
-    @patch('time.sleep')
+    @patch("time.sleep")
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for DB when getting OperationalError"""
-        patched_check.side_effect = [Psycopg2Error] * 2 + \
-            [OperationalError] * 3 + [True]
+        patched_check.side_effect = (
+            [Psycopg2Error] * 2 + [OperationalError] * 3 + [True]
+        )
 
-        call_command('wait_for_db')
+        call_command("wait_for_db")
 
         self.assertEqual(patched_check.call_count, 6)
-        patched_check.assert_called_with(databases=['default'])
+        patched_check.assert_called_with(databases=["default"])
